c6cabf63dddf25133138b145a7a4ac6592df1bc5
leo108 <leo108@qq.com>
2014-07-19 23:50:44 +0800

    fix issue #3

--- app/models/tab_setting.rb
+++ app/models/tab_setting.rb
@@ -1,10 +1,5 @@
 class TabSetting < ActiveRecord::Base
-    @@default_settings = {}
     @@cache = {}
-    Redmine::Plugin.all.each do |plugin|
-        next unless plugin.tab_settings
-        @@default_settings[plugin.id] = plugin.tab_settings[:default]
-    end
     def self.get_settings(project_id)
         return @@cache[project_id.to_s] unless @@cache[project_id.to_s].nil?
         setting_arr = where(:project_id => project_id)
@@ -15,8 +10,8 @@ class TabSetting < ActiveRecord::Base
         Redmine::Plugin.all.each do |plugin|
             next unless plugin.tab_settings
             next if setting_hash.has_key?(plugin.id.to_s)
-            setting_hash[plugin.id.to_s] = @@default_settings[plugin.id]
-            obj = new(:project_id => project_id, :plugin_id => plugin.id, :settings => @@default_settings[plugin.id].to_yaml)
+            setting_hash[plugin.id.to_s] = plugin.tab_settings[:default].dup
+            obj = new(:project_id => project_id, :plugin_id => plugin.id, :settings => plugin.tab_settings[:default].to_yaml)
             obj.save
         end
         @@cache[project_id.to_s] = setting_hash
